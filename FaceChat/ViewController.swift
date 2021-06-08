b598f6f5871edce15a45bc7877904eb8b6c54cd7
rinsuki <428rinsuki+git@gmail.com>
2021-06-08 10:51:03 +0900

    Initial Commit

---
+++ FaceChat/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FaceChat
+//
+//  Created by user on 2021/06/08.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
